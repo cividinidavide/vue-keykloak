2e3fbc2b30d452e54f4571402526272f7506d1c7
Davide Cividini <davide.civid.96@gmail.com>
2019-05-29 15:01:42 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<div v-if="authenticated">
+			<p>Ciao {{ keycloak.tokenParsed.given_name }}, sei loggato!</p>
+			<button @click="logout()">Logout</button>
+		</div>
+		<p v-if="!authenticated">Non sei autenticato!</p>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+	import { Component, Vue } from 'vue-property-decorator';
+	import Keycloak from 'keycloak-js';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+	@Component({})
+	export default class App extends Vue {
+		keycloak: any;
+		authenticated: any = null;
+
+		mounted() {
+			const keycloak = Keycloak('../keykloak.json');
+			keycloak.init({onLoad: 'login-required'}).success(authenticated => {
+				this.keycloak = keycloak;
+				console.log(this.keycloak);
+				this.authenticated = authenticated;
+			})
+		}
+
+		logout() {
+			this.keycloak.logout();
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
